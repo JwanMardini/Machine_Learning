efa6ea27fa08da9e54263fc3aad27d63a5b09b88
Jwan Mardini <jwan.99.mardini@gmail.com>
2024-11-06 21:22:06 +0100

    Update CORS middleware to allow requests from any origin

--- Project_work/ml_api.py
+++ Project_work/ml_api.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Replace with your frontend URL
+    allow_origins=["*"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
